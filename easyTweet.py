8047714aeb18297f6168a40be69968cffd2bba0e
sjpeters2 <shanejpeters1992@gmail.com>
2015-10-23 22:18:38 -0500

    Added more code to main

--- easyTweet.py
+++ easyTweet.py
@@ -1,4 +1,5 @@
 from twitter import *
+from scanner import *
 
 access_token = "" #Third Line in text file
 access_token_secret = "" #Fourth Line in text file
@@ -8,5 +9,14 @@ consumer_key_secret = "" #Second Line in text file
 #t = Twitter(auth=Oauth(access_token,access_token_secret,consumer_key,consumer_key_secret))
 
 
-def openFile(filename):
-    s = Scanner(filename)
+def readFile(filename):
+    fp = open(filename,"r")
+	
+	consumer_key = fp.readline()
+	consumer_key_secret = fp.readline()
+	access_token = fp.readline()
+	access_token_secret = fp.readline()
+	
+	fp.close()
+
+	
